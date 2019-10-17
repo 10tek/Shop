1755ce8ba03eac983971127339bb27fa0538a621
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-17 19:45:18 +0600

    EntityLEvel initial commit

--- Shop.UI/Program.cs
+++ Shop.UI/Program.cs
@@ -10,11 +10,10 @@
  * unit of work
  */
 
-using Airport.DataAccess;
 using Microsoft.Extensions.Configuration;
-using System.Data.Common;
-using System.Data.SqlClient;
+using Shop.DataAccess;
 using Shop.Domain;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
 
@@ -24,41 +23,22 @@ namespace Shop.UI
     {
         static void Main(string[] args)
         {
-            #region
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", false, true);
             IConfigurationRoot configurationRoot = builder.Build();
             var connectionString = configurationRoot.GetConnectionString("DebugConnectionString");
-            var providerName = configurationRoot.GetSection("AppConfig").GetChildren().Single(item => item.Key == "ProviderName").Value;
 
-            DbProviderFactories.RegisterFactory(providerName, SqlClientFactory.Instance);
+            Category category = new Category
+            {
+                Name = "ПК",
+                ImagePath = "C:/data/pc"
+            };
 
-            Repository<Category> repository = new Repository<Category>(connectionString, providerName);
-
-
-
-            #endregion
-            #region Repository Test
-            //Category category2 = new Category
-            //{
-            //    Name = "Мышки",
-            //    ImagePath = @"C:/data",
-            //};
-
-            //Repository<Category> repository = new Repository<Category>(configurationRoot.GetConnectionString("DebugConnectionString"));
-            //repository.Add(category2);
-            //var res = repository.GetAll();
-
-            //Repository<User> repository = new Repository<User>("");
-            //repository.Add(new User
-            //{ 
-            //    Address = "Abay st. 129",
-            //    Email = "10tek3@mail.com",
-            //    Password = "123",
-            //    PhoneNumber = "+77786226134",
-            //    VerificationCode = "123"
-            //});
-            #endregion
+            using(var context = new ShopContext(connectionString))
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
 
 
         }
